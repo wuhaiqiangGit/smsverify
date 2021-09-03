15be9b8de932bbb30a0c37434515692b170094d5
wuhaiqiang <18820185804@163.com>
2021-09-03 21:24:37 +0800

    Initial commit

---
+++ src/main/java/com/whq/mvc/smsverify/SmsverifyApplication.java
@@ -0,0 +1,13 @@
+package com.whq.mvc.smsverify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsverifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsverifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whq/mvc/smsverify/SmsverifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whq.mvc.smsverify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsverifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
